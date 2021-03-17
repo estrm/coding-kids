d21b1d877bfa4191f8a8b2a2be33ea087bc41069
guguant <guguant@yahoo.com>
2021-03-18 01:02:04 +0800

    optimize output result

--- spelling_typo_check/run_spelling_typo_check.sh
+++ spelling_typo_check/run_spelling_typo_check.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-# run_spelling_typo_check.sh
+#
+# (c) 2021, Sun Yiming <zscandyz@gmail.com>
+#
+# This script is a tool that it can check
+# code spelling mistakes and typos automatically.
+#
+# usage: put the 'spelling.txt' and 'run_spelling_typo_check.sh'
+#        under the same directory, and then
+#        Run 'run_spelling_typo_check.sh'
+#
+# version:
+# 2021/03/02, v1.0, first release, Sun Yiming
+# 2021/03/17, v2.0, optimize output result, Sun Yiming 
+#
 # test datasets:
 #   https://github.com/torvalds/linux/blob/master/scripts/spelling.txt
 
@@ -13,17 +26,32 @@ else
 fi
 
 line_sum=`awk 'END {print NR}' $input`
-index=1
-cat $input | while read line
+typo_sum=0
+index=0
+
+while read line
 do
 	echo "[$index/$line_sum] : $line"
 
-	echo $line [$index/$line_sum] >> $output
-	echo "====================" >> $output
-
 	keyword=${line%%|*}
-	grep -rsnwi $keyword --exclude $input >> $output
-	echo >> $output
+	temp_grep_result=""
+	temp_grep_result=$(grep -rsnwi $keyword --exclude $input)
+
+	if [ "$temp_grep_result" != "" ]; then
+		echo ">>>>>> $keyword is matched!"
+
+		echo $line [$index/$line_sum] >> $output
+		echo "====================" >> $output
+		echo $temp_grep_result >> $output
+		echo >> $output
+
+		((typo_sum++))
+	fi
 
 	((index++))
-done
+done < ${input}
+
+echo ""
+echo "####################"
+echo "typo_sum=$typo_sum"
+echo "####################"
